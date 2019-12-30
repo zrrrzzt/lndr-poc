11ba5ba383e4334f52e5df14ec2a8032f5ec1f31
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-30 08:28:22 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+const nanoid = require('nanoid')
+const QRCode = require('qrcode.react')
+
+const HomePage = () => {
+  const thisId = nanoid()
+  return (
+    <>
+      <h1>Velkommen</h1>
+      <QRCode value={`https://lndr.allthethings.win/item/${thisId}`} />
+      <style global jsx>{`
+        body {
+          margin: 0;
+          padding: 0;
+          width: 100%;
+          text-align: center;
+        }
+      `}
+      </style>
+    </>
+  )
+}
+
+export default HomePage
